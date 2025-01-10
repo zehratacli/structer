05d47b5e6c7520f95ff15ea7070ce8384bec75d4
zehra <taclizehra@gmail.com>
2025-01-10 23:53:53 +0300

    Update soru1.cpp

--- structerodevi/soru1.cpp
+++ structerodevi/soru1.cpp
@@ -17,63 +17,63 @@ int main()
 	setlocale(LC_ALL, "Turkish");
 	
 	Otomobil binek,suv,ticari;
-	cout<<"Binek aracýn markasýný giriniz :"<<endl;
+	cout<<"Binek aracÄ±n markasÄ±nÄ± giriniz :"<<endl;
 	getline(cin,binek.marka);
-	cout<<"Binek aracýn modelini giriniz :"<<endl;
+	cout<<"Binek aracÄ±n modelini giriniz :"<<endl;
 	getline(cin,binek.model);
-	cout<<"Binek aracýn yýlýný giriniz :"<<endl;
+	cout<<"Binek aracÄ±n yÄ±lÄ±nÄ± giriniz :"<<endl;
 	cin>>binek.yil;
-	cout<<"Binek aracýn yakýt türünü giriniz giriniz (B,D,G,H):"<<endl;
+	cout<<"Binek aracÄ±n yakÄ±t tÃ¼rÃ¼nÃ¼ giriniz giriniz (B,D,G,H):"<<endl;
 	cin>>binek.yakit;
-	cout<<"Binek aracýn kilometresini giriniz :"<<endl;
+	cout<<"Binek aracÄ±n kilometresini giriniz :"<<endl;
 	cin>>binek.km;
-	cout<<"Binek aracýn fiyatýný giriniz :"<<endl;
+	cout<<"Binek aracÄ±n fiyatÄ±nÄ± giriniz :"<<endl;
 	cin>>binek.fiyat;
 	
-		cout << "\nBinek Araç Bilgileri:" << endl;
-    cout << "Marka: " << binek.marka <<endl<<"Model: " << binek.model <<endl<< "Yýl: " << binek.yil<<endl
-         << "Yakýt: " << binek.yakit<<endl << "KM:" << binek.km <<endl<< "Fiyat: " << binek.fiyat<< " TL" << endl;
+		cout << "\nBinek AraÃ§ Bilgileri:" << endl;
+    cout << "Marka: " << binek.marka <<endl<<"Model: " << binek.model <<endl<< "YÄ±l: " << binek.yil<<endl
+         << "YakÄ±t: " << binek.yakit<<endl << "KM:" << binek.km <<endl<< "Fiyat: " << binek.fiyat<< " TL" << endl;
 	
-	cout<<"\nSuv aracýn markasýný giriniz :"<<endl;
+	cout<<"\nSuv aracÄ±n markasÄ±nÄ± giriniz :"<<endl;
 	getline(cin,suv.model);
 	cin.ignore();
-	cout<<"Suv aracýn modelini giriniz :"<<endl;
+	cout<<"Suv aracÄ±n modelini giriniz :"<<endl;
 	getline(cin,suv.model);
 	cin.ignore();
-	cout<<"Suv aracýn yýlýný giriniz :"<<endl;
+	cout<<"Suv aracÄ±n yÄ±lÄ±nÄ± giriniz :"<<endl;
 	cin>>suv.yil;
-	cout<<"Suv aracýn yakýt türünü giriniz giriniz (B,D,G,H):"<<endl;
+	cout<<"Suv aracÄ±n yakÄ±t tÃ¼rÃ¼nÃ¼ giriniz giriniz (B,D,G,H):"<<endl;
 	cin>>suv.yakit;
-	cout<<"Suv aracýn kilometresini giriniz :"<<endl;
+	cout<<"Suv aracÄ±n kilometresini giriniz :"<<endl;
 	cin>>suv.km;
-	cout<<"Suv aracýn fiyatýný giriniz :"<<endl;
+	cout<<"Suv aracÄ±n fiyatÄ±nÄ± giriniz :"<<endl;
 	cin>>suv.fiyat;
 	
-	 cout << "\nSUV Araç Bilgileri:" << endl;
-    cout << "Marka: " << suv.marka <<endl <<" Model: " << suv.model <<endl <<"Yýl: " << suv.yil<<endl
-         << "Yakýt: " << suv.yakit<<endl << "KM: " << suv.km <<endl <<"Fiyat: " << suv.fiyat << " TL" << endl;
+	 cout << "\nSUV AraÃ§ Bilgileri:" << endl;
+    cout << "Marka: " << suv.marka <<endl <<" Model: " << suv.model <<endl <<"YÄ±l: " << suv.yil<<endl
+         << "YakÄ±t: " << suv.yakit<<endl << "KM: " << suv.km <<endl <<"Fiyat: " << suv.fiyat << " TL" << endl;
          
          
-	cout<<"Ticari aracýn markasýný giriniz :"<<endl;
+	cout<<"Ticari aracÄ±n markasÄ±nÄ± giriniz :"<<endl;
 	getline(cin,ticari.marka);
 	cin.ignore();
-	cout<<"Ticari aracýn modelini giriniz :"<<endl;
+	cout<<"Ticari aracÄ±n modelini giriniz :"<<endl;
 	getline(cin,ticari.model);
 	cin.ignore();
-	cout<<"Ticari aracýn yýlýný giriniz :"<<endl;
+	cout<<"Ticari aracÄ±n yÄ±lÄ±nÄ± giriniz :"<<endl;
 	cin>>ticari.yil;
 	cin.ignore();
-	cout<<"Ticari aracýn yakýt türünü  giriniz (B,D,G,H):"<<endl;
+	cout<<"Ticari aracÄ±n yakÄ±t tÃ¼rÃ¼nÃ¼  giriniz (B,D,G,H):"<<endl;
 	cin>>ticari.yakit;
-	cout<<"Ticari aracýn kilometresini giriniz :"<<endl;
+	cout<<"Ticari aracÄ±n kilometresini giriniz :"<<endl;
 	cin>>ticari.km;
 	cin.ignore();
-	cout<<"Ticari aracýn fiyatýný giriniz :"<<endl;
+	cout<<"Ticari aracÄ±n fiyatÄ±nÄ± giriniz :"<<endl;
 	cin>>ticari.fiyat;
 
-    cout << "\n--- Ticari Araç Bilgileri ---" << endl;
-    cout << "Marka: " << ticari.marka <<endl<< "Model: " << ticari.model<<endl << "Yýl: " << ticari.yil<<endl
-         << "Yakýt: " << ticari.yakit <<endl<< "KM: " << ticari.km <<endl <<"Fiyat: " << ticari.fiyat << " TL" << endl;
+    cout << "\n Ticari AraÃ§ Bilgileri" << endl;
+    cout << "Marka: " << ticari.marka <<endl<< "Model: " << ticari.model<<endl << "YÄ±l: " << ticari.yil<<endl
+         << "YakÄ±t: " << ticari.yakit <<endl<< "KM: " << ticari.km <<endl <<"Fiyat: " << ticari.fiyat << " TL" << endl;
 	
 	return 0;
 }
